f33ebdf1298e55d4b0fe25554cd771c8ba705a4f
이대경 <eorud2546@nate.com>
2023-08-18 01:40:36 +0900

    Update ChatMessageRepository.java

--- demo/src/main/java/com/example/demo/chatt/db/ChatMessageRepository.java
+++ demo/src/main/java/com/example/demo/chatt/db/ChatMessageRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.chatt.db;//package com.ssafy.singstreet.chatt.db;
+package com.example.demo.chatt.db;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -7,5 +7,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 
-    Slice<ChatMessage> findAllByEntId(int entId, Pageable pageable);
+    // Slice<ChatMessage> findAllByEntId(int entId, Pageable pageable);
+    Slice<ChatMessage> findAllByEntId(int entId, PageRequest createdAt);
 }
