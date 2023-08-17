d389cd91e18b6159265fa7c9e72c59e0c0e45cb7
이대경 <eorud2546@nate.com>
2023-08-17 23:18:11 +0900

    Update WebSocketConfig.java

--- demo/src/main/java/com/example/demo/chatt/config/WebSocketConfig.java
+++ demo/src/main/java/com/example/demo/chatt/config/WebSocketConfig.java
@@ -33,7 +33,7 @@ public class WebSocketConfig implements WebSocketConfigurer {//implements WebSoc
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketHandlerImpl(), "ws/chatt/{entId}")
+        registry.addHandler(new WebSocketHandlerImpl(), "/ws/chatt/{entId}")
                 .setAllowedOrigins("*");
     }
 
